75ae69bd90c3717e387b035bff350a201a5cc013
Nat <nathaniel18_2@yahoo.fr>
2021-10-28 17:28:17 +0200

    some cleaning

--- ft_putstr_fd.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-#include "libft.h"
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	while (*s)
-		write(fd, s++, 1);
-}
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("%d %d\n",(char) 'a', (char) 'b');
-	return 0;
-}
